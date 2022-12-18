e0ea2af06613aae0dfb1f26e2b52759f4fce54ad
Bjørnar Borge <72221999+bjoernarborge@users.noreply.github.com>
2022-12-18 20:23:41 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,11 +18,42 @@ const io = new Server(server, {
     },
   });
 
+const CHAT_BOT = 'Chat Bot'; // The name of the chat bot.
+let chatRoom = ''; // The chat room the user is in.
+let allUsers = []; // All users in the chat room.
+
 // Listen for when the client connects via socket.io-client
 io.on('connection', (socket) => {
-    console.log(`User connected ${socket.id}`);
-  
-    // We can write our socket event listeners in here...
+  console.log(`User connected ${socket.id}`);
+
+  // Add a user to the room
+  socket.on('join_room', (data) => {
+    const { username, room } = data; // Data sent from client when join_room event emitted.
+    socket.join(room); // Join the user to a socket room.
+    
+    let __createdtime__ = Date.now(); // Get the current time in milliseconds.
+    // Send message to all users currently in the room, apart from the user who just joined.
+    socket.to(room).emit('receive_message', {
+      message: '${username} has joined the room.',
+      username: CHAT_BOT,
+      __createdtime__,
+    });
+
+    // Send message to the user who just joined the room.
+    socket.emit('receive_message', {
+      message: 'Welcome to the room ${username}.',
+      username: CHAT_BOT,
+      __createdtime__,
+    });
+
+  // Save the new user to the room
+  chatRoom = room;
+  allUsers.push({ id: socket.id, username, room });
+  chatRoomUsers = allUsers.filter((user) => user.room === room);
+  socket.to(room).emit('chatroom_users', chatRoomUsers);
+  socket.emit('chatroom_users', chatRoomUsers);
+
   });
+});
 
 server.listen(4000, () => 'Server is running on port 4000');
