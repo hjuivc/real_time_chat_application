d00baff480ffa29a1b3b21c7d27739ff9bae062e
Bjørnar Borge <72221999+bjoernarborge@users.noreply.github.com>
2022-12-19 10:52:18 +0100

    Update index.js

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,49 +1,49 @@
-// client/src/pages/home/index.js
-
-import styles from './style.module.css';
-import { useNavigate } from 'react-router-dom';
+import styles from './styles.module.css';
+import { useNavigate } from 'react-router-dom'; // Add this
 
 const Home = ({ username, setUsername, room, setRoom, socket }) => {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Add this
 
-  // Create a function for joining a room
   const joinRoom = () => {
-      // If username and room are not empty
-      if (room !== '' && username !== '') {
-          // Emit the joinRoom event
-          socket.emit('joinRoom', { username, room });
-      }
+    if (room !== '' && username !== '') {
+      socket.emit('join_room', { username, room });
+    }
 
-      // Navigate to the chat page
-      navigate('/chat', { replace: true });
+    // Redirect to /chat
+    navigate('/chat', { replace: true });
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
         <h1>{`<>DevRooms</>`}</h1>
-        <input className={styles.input} placeholder='Username...' 
-        onChange={(e) => setUsername(e.target.value)} />
+        <input
+          className={styles.input}
+          placeholder='Username...'
+          onChange={(e) => setUsername(e.target.value)}
+        />
 
-        <select className={styles.input}
-        onChange={(e) => setUsername(e.target.value)}>
+        <select
+          className={styles.input}
+          onChange={(e) => setRoom(e.target.value)}
+        >
           <option>-- Select Room --</option>
           <option value='javascript'>JavaScript</option>
           <option value='node'>Node</option>
           <option value='express'>Express</option>
           <option value='react'>React</option>
         </select>
-          
-          <button // Add a button to join the room. When clicked, call the joinRoom function
-                  className='btn btn-secondary' 
-                  style={{ width: '100%' }}
-                  onClick={joinRoom}>
-                    
-                  Join Room
-          </button>
+
+        <button
+          className='btn btn-secondary'
+          style={{ width: '100%' }}
+          onClick={joinRoom}
+        >
+          Join Room
+        </button>
       </div>
     </div>
-    );
-  };
-  
-  export default Home;
+  );
+};
+
+export default Home;
